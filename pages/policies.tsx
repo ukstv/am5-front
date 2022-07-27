d49daf15ed8962be47a16405b46aaf3931e03b9e
Sergey Ukustov <sergey@ukstv.me>
2022-07-27 21:43:38 +0100

    Interface (#1)
    * wip
    
    * links work
    
    * pages
    
    * iam

---
+++ pages/policies.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import {Container} from "../lib/components/container";
+
+const PoliciesPage: NextPage = () => {
+  return <Container>Policies List</Container>;
+};
+
+export default PoliciesPage;

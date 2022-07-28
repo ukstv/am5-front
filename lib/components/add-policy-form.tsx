b48d8004d17d674284a80707fc6ad4b38d7e2704
Sergey Ukustov <sergey@ukstv.me>
2022-07-28 15:30:26 +0100

    perm1

--- lib/components/add-policy-form.tsx
+++ lib/components/add-policy-form.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useState } from "react";
 import { CacaoBlock } from "ceramic-cacao";
 import { useSiwe } from "../fancy/use-siwe";
+import { createResource } from "../perm1";
 
 type Props = {
   onSuccess: (block: CacaoBlock) => void;
@@ -13,7 +14,13 @@ export function AddPolicyForm(props: Props) {
   const handleAddPolicy = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const block = await siweFn({ uri: subject });
+      const resource = createResource(
+        "http://roadmap.ceramic.network",
+        "read",
+        "ceramic://kjz",
+        "$.policy"
+      );
+      const block = await siweFn({ uri: subject, resources: [resource] });
       props.onSuccess(block);
     } catch (e: any) {
       props.onError?.(e);
